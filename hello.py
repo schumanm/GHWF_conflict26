fb3533b348a168264f789cd9b707abd3e3a2a04a
maxwellschuman <IaPIjDKh>
2021-04-23 10:34:13 -0400

    moremorepractice

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX
 
 
 This is me in a new branch editing the file for kyle 
-
-I want you to delete this line of code ofr the coflict and add a new line
+This is more code fluff that i want to cause a conflict
+I want you to delete this line of code ofr the coflict and new line
