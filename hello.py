0549a7b58852f5b4799980301c19d85cd8ea7555
maxwellschuman <IaPIjDKh>
2021-04-22 21:59:26 -0400

    moooooorestuffffff

--- hello.py
+++ hello.py
@@ -2,18 +2,4 @@ name = input('Name: ')
 print('Hello, {}!'.format(name))
 
 KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX
-
-
-This is me in a new branch editing the file for kyle 
-/
-/
-/
-/
-/
-/
-/
-/
-/
-/
-//
-hahhaahahhhahahahahahahahahah
+WOOOHOOOO
