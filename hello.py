e96566d73079c17f64aabf17e83037e8dcb740a5
Kyle Smilon <smilonk@moravian.edu>
2021-04-23 09:46:22 -0400

    no

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 name = input('Name: ')
 print('Hello!, {}!'.format(name))
 
-KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX
+KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX, no-kyle
 
 
 This is me in a new branch editing the file for kyle 
-/
-/
-/
+/h
+/h
+/h
 /hahhaahahhhahahahahahahahahah
