90f9f0ea5c5696e2be09928d0bd3044dd96f0aad
maxwellschuman <IaPIjDKh>
2021-04-22 22:12:25 -0400

    wohoo

--- hello.py
+++ hello.py
@@ -5,7 +5,5 @@ KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX
 
 
 This is me in a new branch editing the file for kyle 
-/
-/
-/
-/hahhaahahhhahahahahahahahahah
+
+I want you to delete this line of code ofr the coflict and add a new line
