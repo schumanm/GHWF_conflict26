18b1b6cfd992fbc623265720b627c50ac570abe7
maxwellschuman <IaPIjDKh>
2021-04-23 10:36:06 -0400

    hello

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 name = input('Name: ')
 print('Hello!, {}!'.format(name))
 
-KYLEEEEEEE THIS IS THE ERROR I WANT YOU TO FIX
 
-
-This is me in a new branch editing the file for kyle 
-This is more code fluff that i want to cause a conflict
-I want you to delete this line of code ofr the coflict and new line
