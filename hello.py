05716da97a38c6baa922ac6b20cf1d87ece7ef67
maxwellschuman <IaPIjDKh>
2021-04-23 11:14:23 -0400

    hellooo

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-
-=======
-
 
 name = input('Name: ')
-IM EDITING THIS LINE TO FORCE A ISSUEprint('Hello!, {}!'.format(name)
->>>>>>> 1315d3f6ff018648a5c2978ef90ad161090d6894
-
+print('Hello!, {}!'.format(name)
 
